63265d83e58517ca658dc8e556ea03f3a7c1aa36
spenning <whimizera@mailbox.org>
2024-05-16 13:29:00 +0200

    ft_calloc normproof

--- tests/libft/libft_tests/ft_calloc_test.c
+++ tests/libft/libft_tests/ft_calloc_test.c
@@ -1,83 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 13:12:54 by spenning          #+#    #+#             */
+/*   Updated: 2024/05/16 13:28:27 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft_tester.h"
 #include <string.h>
 
-int fail_calloc = 0;
+int	g_fc = 0;
 
-int calloc_cmp(int test_count, size_t nmemb, size_t n)
+char	*init_ft_calloc(size_t nmemb, size_t n)
 {
-    FILE 	*errorLog;
-    char * calloc_vptr;
-    char * ft_calloc_vptr;
-    size_t error_index;
+	char	*ftc;
 
-    error_index = 0;
-    errorLog = fopen("logs/error_log.txt", "a");
-    if (errorLog == NULL)
-    {   
-        printf("Error opening log file\n");
-        return 1;
-    }
-
-    calloc_vptr = calloc(nmemb, n);
-    if(calloc_vptr == NULL)
-    {
-        printf("calloc error\n");
-        return 1;
-    }
-    ft_calloc_vptr = ft_calloc(nmemb, n);
-    if(ft_calloc_vptr == NULL)
-    {
-        if(calloc_vptr)
-            free(calloc_vptr);
-        printf("ft_calloc error\n");
-        return 1;
-    }
-
-    while(error_index != (nmemb * n))
-    {
-        if((char)calloc_vptr[error_index] != (char)ft_calloc_vptr[error_index]) 
-        {
-            printf(RED "%d FAIL "RESET, test_count);
-            fprintf(errorLog,"error\n");
-            fprintf(errorLog,"test number: %d\n", test_count);
-            fprintf(errorLog,"test case: nmemb %ld n %ld\n", nmemb, n);
-            fprintf(errorLog,"calloc: %c\n", (char)calloc_vptr[error_index]);
-            fprintf(errorLog,"ft_calloc: %c\n", (char)ft_calloc_vptr[error_index]);
-            fprintf(errorLog,"---------\n");
-            fail_calloc += 1;
-        }
-        error_index++;
-    }
-    printf(GRN "%d OK " RESET, test_count);
-    free(calloc_vptr);
-    free(ft_calloc_vptr);
-    return(test_count + 1);
+	ftc = ft_calloc(nmemb, n);
+	if (ftc == NULL)
+	{
+		printf("calloc error\n");
+		return (NULL);
+	}
+	return (ftc);
 }
 
-int calloc_test()
+char	*init_calloc(size_t nmemb, size_t n)
 {
-    int  test_count = 1;
-    printf("\n");
+	char	*c;
+
+	c = calloc(nmemb, n);
+	if (c == NULL)
+	{
+		printf("calloc error\n");
+		return (NULL);
+	}
+	return (c);
+}
+
+FILE	*init_elog(void)
+{
+	FILE	*elog;
+
+	elog = fopen("logs/error_log.txt", "a");
+	if (elog == NULL)
+	{
+		printf("Error opening log file\n");
+		return (NULL);
+	}
+	return (elog);
+}
+
+int	calloc_cmp(int test_count, size_t nmemb, size_t n)
+{
+	FILE	*elog;
+	char	*c;
+	char	*ftc;
+	size_t	ei;
+
+	ei = 0;
+	elog = init_elog();
+	c = init_calloc(nmemb, n);
+	ftc = init_ft_calloc(nmemb, n);
+	if (elog == NULL || c == NULL || ftc == NULL)
+		return (1);
+	while (ei != (nmemb * n))
+	{
+		if ((char)c[ei] != (char)ftc[ei])
+			g_fc += ft_log_chr(test_count, elog, (char)c[ei], (char)ftc[ei]);
+		ei++;
+	}
+	printf(GRN "%d OK " RESET, test_count);
+	free(c);
+	free(ftc);
+	fclose(elog);
+	return (test_count + 1);
+}
+
+int	calloc_test(void)
+{
+	int	test_count;
+
+	test_count = 1;
+	printf("\n");
 	printf(BMAG "ft_calloc\n" RESET);
-    test_count = calloc_cmp(test_count, 1, sizeof(char));
-    test_count = calloc_cmp(test_count, 1000, sizeof(char));
-    test_count = calloc_cmp(test_count, 1000000, sizeof(char));
-    test_count = calloc_cmp(test_count, 1, sizeof(int));
-    test_count = calloc_cmp(test_count, 1000, sizeof(int));
-    test_count = calloc_cmp(test_count, 1000000, sizeof(int));
-    test_count = calloc_cmp(test_count, 1, sizeof(float));
-    test_count = calloc_cmp(test_count, 1000, sizeof(float));
-    test_count = calloc_cmp(test_count, 1000000, sizeof(float));
-    test_count = calloc_cmp(test_count, 1, sizeof(void));
-    test_count = calloc_cmp(test_count, 1000, sizeof(void));
-    test_count = calloc_cmp(test_count, 1000000, sizeof(void));
-    test_count = calloc_cmp(test_count, 1, 0);
-    test_count = calloc_cmp(test_count, 1000, 0);
-    test_count = calloc_cmp(test_count, 1000000, 0);
-    test_count = calloc_cmp(test_count, 0,1);
-    test_count = calloc_cmp(test_count, 0,1000);
-    test_count = calloc_cmp(test_count, 0,1000000);
-    return(fail_calloc);
+	test_count = calloc_cmp(test_count, 1, sizeof(char));
+	test_count = calloc_cmp(test_count, 1000, sizeof(char));
+	test_count = calloc_cmp(test_count, 1000000, sizeof(char));
+	test_count = calloc_cmp(test_count, 1, sizeof(int));
+	test_count = calloc_cmp(test_count, 1000, sizeof(int));
+	test_count = calloc_cmp(test_count, 1000000, sizeof(int));
+	test_count = calloc_cmp(test_count, 1, sizeof(float));
+	test_count = calloc_cmp(test_count, 1000, sizeof(float));
+	test_count = calloc_cmp(test_count, 1000000, sizeof(float));
+	test_count = calloc_cmp(test_count, 1, sizeof(void));
+	test_count = calloc_cmp(test_count, 1000, sizeof(void));
+	test_count = calloc_cmp(test_count, 1000000, sizeof(void));
+	test_count = calloc_cmp(test_count, 1, 0);
+	test_count = calloc_cmp(test_count, 1000, 0);
+	test_count = calloc_cmp(test_count, 1000000, 0);
+	test_count = calloc_cmp(test_count, 0, 1);
+	test_count = calloc_cmp(test_count, 0, 1000);
+	test_count = calloc_cmp(test_count, 0, 1000000);
+	return (g_fc);
 }
-
-
--- tests/libft/libft_tests/ft_log_util.c
+++ tests/libft/libft_tests/ft_log_util.c
@@ -6,11 +6,22 @@
 /*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:21:40 by spenning          #+#    #+#             */
-/*   Updated: 2024/05/16 13:05:44 by spenning         ###   ########.fr       */
+/*   Updated: 2024/05/16 13:18:11 by spenning         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_tester.h"
+
+int ft_log_chr(int test_count, FILE *errorlog, char ro, char rft)
+{
+	printf(RED "%d FAIL "RESET, test_count);
+	fprintf(errorlog, "error\n");
+	fprintf(errorlog, "test number: %d\n", test_count);
+	fprintf(errorlog, "org: %c\n", ro);
+	fprintf(errorlog, "ft: %c\n", rft);
+	fprintf(errorlog, "---------\n");
+	return (1);
+}
 
 int ft_log_str(int test_count, FILE *errorlog, char* ro, char* rft)
 {
