183ef61b16dbcf2a481e5f49bfa9d27f823077d6
sebastianpenning <whimizera@mailbox.org>
2024-05-17 06:31:09 +0200

    ft_strlen test normproof

--- tests/libft/libft_tests/ft_strlen_test.c
+++ tests/libft/libft_tests/ft_strlen_test.c
@@ -1,54 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 06:27:38 by mynodeus          #+#    #+#             */
+/*   Updated: 2024/05/17 06:30:10 by mynodeus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft_tester.h"
 #include <string.h>
 
-int fail_strlen = 0;
+int	g_fail_strlen = 0;
 
-int strlen_cmp(int test_count, char *test)
+int	strlen_cmp(int test_count, char *test)
 {
-    FILE 	*errorLog;
-    size_t result_ft;
-	size_t result_org;
+	FILE	*errorlog;
+	size_t	ft;
+	size_t	org;
 
-    errorLog = fopen("logs/error_log.txt", "a");
-    if (errorLog == NULL)
-    {
-        printf("Error opening log file\n");
-        return 1;
-    }
-    result_org = strlen(test);
-	result_ft = ft_strlen(test);
-    if(result_ft != result_org) 
-    {
-        printf(RED "%d FAIL "RESET, test_count);
-        fprintf(errorLog,"error\n");
-        fprintf(errorLog,"test number: %d\n", test_count);
-        fprintf(errorLog,"test case: %s\n", test);
-        fprintf(errorLog,"strlen: %zu\n", result_org);
-        fprintf(errorLog,"ft_strlen: %zu\n", result_ft);
-        fprintf(errorLog,"---------\n");
-        fail_strlen += 1;
-    }
-    else
+	errorlog = fopen("logs/error_log.txt", "a");
+	if (errorlog == NULL)
+	{
+		printf("Error opening log file\n");
+		return (1);
+	}
+	org = strlen(test);
+	ft = ft_strlen(test);
+	if (ft != org)
+		g_fail_strlen += ft_log_int(test_count, errorlog, org, ft);
+	else
 		printf(GRN "%d OK " RESET, test_count);
-    return(test_count + 1);
+	return (test_count + 1);
 }
 
-int strlen_test()
+int	strlen_test(void)
 {
-    int  test_count = 1;
+	int	test_count;
 
-    printf("\n");
+	test_count = 1;
+	printf("\n");
 	printf(BMAG "ft_strlen\n" RESET);
-    test_count = strlen_cmp(test_count, "nfdsnkjd");
-    test_count = strlen_cmp(test_count, "bobobbocob");
-    test_count = strlen_cmp(test_count, "a");
-    test_count = strlen_cmp(test_count, "dfsfdsf?");
-    test_count = strlen_cmp(test_count, "");
-    test_count = strlen_cmp(test_count, " ");
-    return(fail_strlen);
+	test_count = strlen_cmp(test_count, "nfdsnkjd");
+	test_count = strlen_cmp(test_count, "bobobbocob");
+	test_count = strlen_cmp(test_count, "a");
+	test_count = strlen_cmp(test_count, "dfsfdsf?");
+	test_count = strlen_cmp(test_count, "");
+	test_count = strlen_cmp(test_count, " ");
+	return (g_fail_strlen);
 }
-
-
-
-
-
