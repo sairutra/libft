89c7b56e5598b723c116008b094d0252f88d0c77
sebastianpenning <whimizera@mailbox.org>
2024-05-17 05:27:26 +0200

    putstr normproof

--- tests/libft/libft_tests/ft_putstr_test.c
+++ tests/libft/libft_tests/ft_putstr_test.c
@@ -1,121 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 17:08:13 by spenning          #+#    #+#             */
+/*   Updated: 2024/05/17 05:26:50 by mynodeus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft_tester.h"
 #include <string.h>
 #include <fcntl.h>
 
-int fail_putstr = 0;
+int	g_fail_putstr = 0;
 
-int compareFile_putstr(FILE * fPtr1, char *test, int * line, int * col)
+int	comparefile_putstr(FILE *fPtr1, char *test, int *line, int *col)
 {
-    char ch1, ch2;
-	int index;
+	char	ch1;
+	char	ch2;
+	int		index;
 
 	index = 0;
 	*line = 1;
-    *col  = 0;
+	*col = 0;
 	ch1 = '\0';
-
-    // Input character from both files
-    while(ch1 != EOF)
+	while (ch1 != EOF)
 	{
-		if (ch1 != ch2)
-			return -1;
 		ch1 = fgetc(fPtr1);
 		ch2 = test[index];
-	    // If characters are not same then return -1
+		if (ch1 == EOF)
+			return (0);
+		if (ch1 != ch2)
+			return (-1);
 		index++;
 	}
-    return 0;
+	return (0);
 }
 
-int compare_files_putstr(int test_count, char *test)
+int	compare_files_str(int test_count, char *test)
 {
-	int diff;
-	int line, col;
-	char s;
-	FILE * fPtr1; 
-	FILE 	*errorLog;
+	int		line;
+	int		col;
+	FILE	*fptr;
+	FILE	*errorlog;
 
-
-
-	fPtr1 = fopen("libft_tests/putstr_ft.txt", "r");
-    errorLog = fopen("logs/error_log.txt", "a");
-    if (errorLog == NULL)
-    {
-        printf("Error opening log file\n");
-        return 1;
-    }
-	s = '0';
-
-    /* fopen() return NULL if unable to open file in given mode. */
-    if (fPtr1 == NULL)
-    {
-        /* Unable to open file hence exit */
-        printf("\nUnable to open file.\n");
-        printf("Please check whether file exists and you have read privilege.\n");
-        return(-1);
-    }
-    /* Call function to compare file */
-	diff = compareFile_putstr(fPtr1, test, &line, &col);
-	
-	rewind(fPtr1);
-    if (diff != 0)
-    {
-        printf(RED "%d FAIL "RESET, test_count);
-		fprintf(errorLog,"---------\n");
-		fprintf(errorLog,"File not equal error\n");
-		fprintf(errorLog,"test: %d\n", test_count);
-		fprintf(errorLog,"testcase: %s\n", test);
-        fprintf(errorLog,"Line: %d, col: %d\n", line, col);
-		fprintf(errorLog,"function\n");
-		while((s=fgetc(fPtr1))!=EOF) 
-		{
-      		fprintf(errorLog,"%c",s);
-   		}
-		fprintf(errorLog,"\n");
-		fprintf(errorLog,"---------\n");
-        fail_putstr += 1;
-    }
-    /* Finally close files to release resources */
-    fclose(fPtr1);
-	fclose(errorLog);
-    return 0;
+	fptr = fopen("libft_tests/putstr_ft.txt", "r");
+	errorlog = fopen("logs/error_log.txt", "a");
+	if (fptr == NULL || errorlog == NULL)
+	{
+		printf("\nUnable to open file.\n");
+		return (-1);
+	}
+	if (comparefile_putstr(fptr, test, &line, &col) != 0)
+		g_fail_putstr += ft_log_int(test_count, errorlog, line, col);
+	rewind(fptr);
+	fclose(fptr);
+	fclose(errorlog);
+	return (0);
 }
 
-
-
-int putstr_cmp(int test_count, char* test)
+int	putstr_cmp(int test_count, char *test)
 {
-    int fd;
-    fclose(fopen("libft_tests/putstr_ft.txt", "w"));
-    fd = open("libft_tests/putstr_ft.txt", O_RDWR);
-    ft_putstr_fd(test, fd);
-    close(fd);
-    compare_files_putstr(test_count, test);
-    if(fail_putstr == 0)
+	int	fd;
+
+	fclose(fopen("libft_tests/putstr_ft.txt", "w"));
+	fd = open("libft_tests/putstr_ft.txt", O_RDWR);
+	ft_putstr_fd(test, fd);
+	close(fd);
+	compare_files_str(test_count, test);
+	if (g_fail_putstr == 0)
 		printf(GRN "%d OK " RESET, test_count);
-    return(test_count + 1);
+	return (test_count + 1);
 }
 
-int putstr_test()
+int	putstr_test(void)
 {
-    int  test_count = 1;
-    printf("\n");
+	int	test_count;
+
+	test_count = 1;
+	printf("\n");
 	printf(BMAG "ft_putstr\n" RESET);
-    test_count = putstr_cmp(test_count, "a");
-    test_count = putstr_cmp(test_count, "b");
-    test_count = putstr_cmp(test_count, "\200");
-    test_count = putstr_cmp(test_count, "\n");
-    test_count = putstr_cmp(test_count, "%");
-    test_count = putstr_cmp(test_count, " ");
-    test_count = putstr_cmp(test_count, "_");
+	test_count = putstr_cmp(test_count, "a");
+	test_count = putstr_cmp(test_count, "b");
+	test_count = putstr_cmp(test_count, "\200");
+	test_count = putstr_cmp(test_count, "\n");
+	test_count = putstr_cmp(test_count, "%");
+	test_count = putstr_cmp(test_count, " ");
+	test_count = putstr_cmp(test_count, "_");
 	test_count = putstr_cmp(test_count, "safds_");
-    return(fail_putstr);
+	return (g_fail_putstr);
 }
-
-
-
-
-
-
-
-
