eaef45d293cff86cd36d4cff7980d2684103c1ae
sebastianpenning <whimizera@mailbox.org>
2024-05-17 06:45:25 +0200

    strnstr normproof

--- tests/libft/libft_tests/ft_strnstr_test.c
+++ tests/libft/libft_tests/ft_strnstr_test.c
@@ -1,57 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr_test.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 06:27:38 by mynodeus          #+#    #+#             */
+/*   Updated: 2024/05/17 06:45:03 by mynodeus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft_tester.h"
 #include <bsd/string.h>
 
-int fail_strnstr = 0;
+int	g_fail_strnstr = 0;
 
-int strnstr_cmp(int test_count, char *test1, char *test2, size_t n)
+int	strnstr_cmp(int test_count, char *test1, char *test2, size_t n)
 {
-    FILE 	*errorLog;
-    char * result_ft;
-	char * result_org;
+	FILE	*errorlog;
+	char	*ft;
+	char	*org;
 
-    errorLog = fopen("logs/error_log.txt", "a");
-    if (errorLog == NULL)
-    {
-        printf("Error opening log file\n");
-        return 1;
-    }
-    result_org = strnstr(test1, test2, n);
-	result_ft = ft_strnstr(test1, test2, n);
-    if(result_ft != result_org) 
-    {
-        printf(RED "%d FAIL "RESET, test_count);
-        fprintf(errorLog,"error\n");
-        fprintf(errorLog,"test number: %d\n", test_count);
-        fprintf(errorLog,"test case: %s %s\n", test1, test2);
-        fprintf(errorLog,"n: %zu\n", n);
-        fprintf(errorLog,"strnstr: %s\n", result_org);
-        fprintf(errorLog,"ft_strnstr: %s\n", result_ft);
-        fprintf(errorLog,"---------\n");
-        fail_strnstr += 1;
-    }
-    else
+	errorlog = fopen("logs/error_log.txt", "a");
+	if (errorlog == NULL)
+	{
+		printf("Error opening log file\n");
+		return (1);
+	}
+	org = strnstr(test1, test2, n);
+	ft = ft_strnstr(test1, test2, n);
+	if (ft != org)
+		g_fail_strnstr += ft_log_str(test_count, errorlog, org, ft);
+	else
 		printf(GRN "%d OK " RESET, test_count);
-    return(test_count + 1);
+	return (test_count + 1);
 }
 
-int strnstr_test()
+int	strnstr_test(void)
 {
-    int  test_count = 1;
+	int	test_count;
 
-    printf("\n");
+	test_count = 1;
+	printf("\n");
 	printf(BMAG "ft_strnstr\n" RESET);
-    test_count = strnstr_cmp(test_count, "nfdsnkjd", "dlkdsdbs", 16);
-    test_count = strnstr_cmp(test_count, "bobobbocob", "dlksadob", 18);
-    test_count = strnstr_cmp(test_count, "a", "b", 2);
-    test_count = strnstr_cmp(test_count, "dfsfdsf?", "??", 17);
-    test_count = strnstr_cmp(test_count, "", "", 0);
-    test_count = strnstr_cmp(test_count, " ", " ",  2);
-    return(fail_strnstr);
+	test_count = strnstr_cmp(test_count, "nfdsnkjd", "dlkdsdbs", 16);
+	test_count = strnstr_cmp(test_count, "bobobbocob", "dlksadob", 18);
+	test_count = strnstr_cmp(test_count, "a", "b", 2);
+	test_count = strnstr_cmp(test_count, "dfsfdsf?", "??", 17);
+	test_count = strnstr_cmp(test_count, "", "", 0);
+	test_count = strnstr_cmp(test_count, " ", " ", 2);
+	return (g_fail_strnstr);
 }
-
-
-
-
-
-
-
